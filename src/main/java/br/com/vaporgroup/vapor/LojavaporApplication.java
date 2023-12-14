d1d77b81eb0e60c90ecef18a3c0f5e511ae9bd6d
Breno Menezes <breno.luizmenezes0123@gmail.com>
2023-12-14 11:42:52 -0300

    Aplication properties configurado

---
+++ src/main/java/br/com/vaporgroup/vapor/LojavaporApplication.java
@@ -0,0 +1,13 @@
+package br.com.vaporgroup.vapor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojavaporApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojavaporApplication.class, args);
+	}
+
+}
